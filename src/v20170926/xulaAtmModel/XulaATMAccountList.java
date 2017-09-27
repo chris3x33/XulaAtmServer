b30170fd6182f2ae784999b33fcbe813f888a6de
chris3x33 <chris3x33g@gmail.com>
2017-09-27 13:28:21 -0500

    Added methods to XulaATMAccountList

--- src/v20170926/xulaAtmModel/XulaATMAccountList.java
+++ src/v20170926/xulaAtmModel/XulaATMAccountList.java
@@ -3,4 +3,28 @@ package v20170926.xulaAtmModel;
 public class XulaATMAccountList {
 
     private XulaATMAccount[] atmAccounts;
+
+    public double getAccountBalance(long accountID) {
+
+        return -1;
+
+    }
+
+    public WithdrawResult withdraw(long fromAccountId, double
+            withdrawAmount) {
+
+        return null;
+    }
+
+    public DepositResult deposit(long toAccountId, double depositAmount) {
+
+        return null;
+    }
+
+    public TransferResult transfer(long fromAccountId, long toAccount, double transferAmount) {
+
+        return null;
+    }
+
+
 }
