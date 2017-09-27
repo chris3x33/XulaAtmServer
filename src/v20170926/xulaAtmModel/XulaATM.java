832bcd696defd0f17c3389b30fc0031d0550afff
chris3x33 <chris3x33g@gmail.com>
2017-09-26 22:16:09 -0500

    Added methods to XulaATM

--- src/v20170926/xulaAtmModel/XulaATM.java
+++ src/v20170926/xulaAtmModel/XulaATM.java
@@ -1,4 +1,17 @@
 package v20170926.xulaAtmModel;
 
 public class XulaATM {
+
+    public double getAccountBalance(long accountID){
+
+        return -1;
+
+    }
+
+    public long[] getAccountIDs(String userName){
+
+        return new long[0];
+
+    }
+
 }
