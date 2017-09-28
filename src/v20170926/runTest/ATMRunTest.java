484fb81f0564b76d030fe7e9c7a78a89d005c825
chris3x33 <chris3x33g@gmail.com>
2017-09-28 09:33:16 -0500

    Added XulaATM to ATMRunTest

--- src/v20170926/runTest/ATMRunTest.java
+++ src/v20170926/runTest/ATMRunTest.java
@@ -1,8 +1,14 @@
 package v20170926.runTest;
 
+import v20170926.xulaAtmModel.XulaATM;
+
 public class ATMRunTest {
 
     public static void main(String[] args) {
+
+        XulaATM xulaATM;
+
+
 
     }
 
