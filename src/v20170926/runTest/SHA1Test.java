4b9bbcbf9fd368b2375483e8c97b3721f12fa062
chris3x33 <chris3x33g@gmail.com>
2017-09-29 17:03:04 -0500

    Created encrypt(password:String):String in SHA1Test

--- src/v20170926/runTest/SHA1Test.java
+++ src/v20170926/runTest/SHA1Test.java
@@ -1,7 +1,14 @@
 package v20170926.runTest;
 
 public class SHA1Test {
+
     public static void main(String[] args) {
 
     }
+
+    private static String encrypt(String password){
+
+        return null;
+    }
+
 }
