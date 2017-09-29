e40ce8c1e1cc9c0beb145509318328bd26d8c629
chris3x33 <chris3x33g@gmail.com>
2017-09-29 17:14:47 -0500

    Added throws NoSuchAlgorithmException to encrypt(password:String):String

--- src/v20170926/runTest/SHA1Test.java
+++ src/v20170926/runTest/SHA1Test.java
@@ -1,17 +1,21 @@
 package v20170926.runTest;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class SHA1Test {
 
     public static void main(String[] args) {
 
     }
 
-    private static String encrypt(String password){
+    private static String encrypt(String password) throws NoSuchAlgorithmException {
 
         //Set encrypt type
         final String encryptType = "SHA-1";
 
         //initialize encryption
+
 
         //get password bytes
 
