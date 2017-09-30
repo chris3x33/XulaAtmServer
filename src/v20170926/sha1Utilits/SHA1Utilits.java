c138035f41f5ea3891d068f38a0c43792011a9f9
chris3x33 <chris3x33g@gmail.com>
2017-09-29 19:14:10 -0500

    Created encrypt(password:String):String in SHA1Utilits

--- src/v20170926/sha1Utilits/SHA1Utilits.java
+++ src/v20170926/sha1Utilits/SHA1Utilits.java
@@ -1,6 +1,28 @@
 package v20170926.sha1Utilits;
 
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class SHA1Utilits {
+
+    public String encrypt(String password) throws NoSuchAlgorithmException {
+
+        //Set encrypt type
+        final String encryptType = "SHA-1";
+
+        //initialize encryption
+        MessageDigest encryptor = MessageDigest.getInstance(encryptType);
+        encryptor.reset();
+
+        //get password bytes
+        byte[] passwordBytes = password.getBytes();
+
+        //encrypt password bytes
+        byte[] encryptedBytes = encryptor.digest(passwordBytes);
+
+        //convert bytes to hex
+        return toHexString(encryptedBytes);
+    }
 
     private String toHexString(byte[] bytes){
 
