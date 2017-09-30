4f9f121a4767ae1bae33d6ad6b49d305e1b879d8
chris3x33 <chris3x33g@gmail.com>
2017-09-29 19:04:56 -0500

    Added toHexString(bytes:byte[]):String to SHA1Utilits

--- src/v20170926/sha1Utilits/SHA1Utilits.java
+++ src/v20170926/sha1Utilits/SHA1Utilits.java
@@ -1,4 +1,18 @@
 package v20170926.sha1Utilits;
 
 public class SHA1Utilits {
+
+    private String toHexString(byte[] bytes){
+
+        StringBuilder hexStringBuilder = new StringBuilder();
+
+        for (byte b : bytes){
+            hexStringBuilder.append(
+                    String.format("%02x", b)
+            );
+        }
+
+        return hexStringBuilder.toString().toUpperCase();
+
+    }
 }
