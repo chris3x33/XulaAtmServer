e9b6aba605528ce247addb22da24ab2e2835d68b
chris3x33 <chris3x33g@gmail.com>
2017-10-04 17:04:10 -0500

    Created ClientHandler

---
+++ src/v20170926/xulaAtmServerController/ClientHandler.java
@@ -0,0 +1,17 @@
+package v20170926.xulaAtmServerController;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientHandler implements Runnable {
+
+    public ClientHandler(Socket socket) throws IOException {
+
+    }
+
+    @Override
+    public void run() {
+
+    }
+
+}
