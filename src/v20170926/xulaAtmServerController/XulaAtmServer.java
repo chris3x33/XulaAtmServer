b5c17c33ea41032da61a0ec2f38d8532404dec4b
chris3x33 <chris3x33g@gmail.com>
2017-10-04 17:07:39 -0500

    Created XulaAtmServer

---
+++ src/v20170926/xulaAtmServerController/XulaAtmServer.java
@@ -0,0 +1,57 @@
+package v20170926.xulaAtmServerController;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketTimeoutException;
+
+public class XulaAtmServer {
+
+    private static final int PORT = 55555;
+    private static final String IP_ADDRESS = /*"127.0.0.1"*/ "192.168.1.74";
+    private static final int TIMEOUT = 100;
+
+    public static void main(String[] args) {
+
+        try {
+
+            ServerSocket server = setupServer();
+
+            runServer(server);
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private static void runServer(ServerSocket server) throws IOException {
+        System.out.println("Server Running");
+        while (true) {
+
+            try {
+
+                Socket socket = server.accept();
+                System.out.println("Accepted Connection From "+socket.getRemoteSocketAddress());
+                Thread clientHandlerThread = new Thread(new ClientHandler(socket));
+                clientHandlerThread.start();
+
+            } catch (SocketTimeoutException e) {
+            }
+        }
+
+    }
+
+    private static ServerSocket setupServer() throws IOException {
+
+        System.out.println("Setting up Server");
+
+        ServerSocket server = new ServerSocket(PORT, 50, InetAddress.getByName(IP_ADDRESS));
+        server.setSoTimeout(TIMEOUT);
+
+        return server;
+
+    }
+
+}
