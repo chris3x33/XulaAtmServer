3cb45bf7818a3aa2dd55bdd118a931a46573b726
chris3x33 <chris3x33g@gmail.com>
2017-10-06 14:41:58 -0500

    Created Global sessionList:SessionList in SessionTest

--- src/v20170926/runTest/SessionTest.java
+++ src/v20170926/runTest/SessionTest.java
@@ -1,6 +1,10 @@
 package v20170926.runTest;
 
+import v20170926.xulaAtmServerController.SessionList;
+
 public class SessionTest {
+
+    private static SessionList sessionList = new SessionList();
 
     public static void main(String[] args) {
 
