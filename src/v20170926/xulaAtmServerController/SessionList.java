4d834cd2d67f6725a81ca7c755566536f72cf1b5
chris3x33 <chris3x33g@gmail.com>
2017-10-06 13:58:55 -0500

    Created Global sessions:LinkedList<Session> in SessionList

--- src/v20170926/xulaAtmServerController/SessionList.java
+++ src/v20170926/xulaAtmServerController/SessionList.java
@@ -1,4 +1,9 @@
 package v20170926.xulaAtmServerController;
 
+import java.util.LinkedList;
+
 public class SessionList {
+
+    private LinkedList<Session> sessions = new LinkedList<Session>();
+
 }
