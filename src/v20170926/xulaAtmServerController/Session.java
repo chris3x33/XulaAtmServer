c7d45f29e597c381a6e2c8dc0ec36094ea143bb6
chris3x33 <chris3x33g@gmail.com>
2017-10-06 13:50:55 -0500

    Created Getters and Setters in Session

--- src/v20170926/xulaAtmServerController/Session.java
+++ src/v20170926/xulaAtmServerController/Session.java
@@ -5,4 +5,19 @@ public class Session {
     private int sessionId;
     private int userId = -1;
 
+    public int getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(int sessionId) {
+        this.sessionId = sessionId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
 }
