6e5baa2f6a2bec26457ce2403475e52dc7e6cb27
chris3x33 <chris3x33g@gmail.com>
2017-10-06 20:46:24 -0500

    Created Global DATA_OUT:DataOutputStream in ClientHandler

--- src/v20170926/xulaAtmServerController/ClientHandler.java
+++ src/v20170926/xulaAtmServerController/ClientHandler.java
@@ -1,6 +1,7 @@
 package v20170926.xulaAtmServerController;
 
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
@@ -11,11 +12,13 @@ public class ClientHandler implements Runnable {
     private final Socket SOCKET;
     private final InputStream IN;
     private final DataInputStream DATA_IN;
+    private final DataOutputStream DATA_OUT;
 
     public ClientHandler(Socket socket) throws IOException {
         SOCKET = socket;
         IN = socket.getInputStream();
         DATA_IN = new DataInputStream(IN);
+        DATA_OUT = new DataOutputStream(socket.getOutputStream());
     }
 
     @Override
