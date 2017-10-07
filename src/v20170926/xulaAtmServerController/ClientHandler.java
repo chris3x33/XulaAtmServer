81581b3e43f832872bab0ac37b02ba041798ee16
chris3x33 <chris3x33g@gmail.com>
2017-10-06 20:39:22 -0500

    Created Global IN:InputStream in ClientHandler

--- src/v20170926/xulaAtmServerController/ClientHandler.java
+++ src/v20170926/xulaAtmServerController/ClientHandler.java
@@ -1,15 +1,18 @@
 package v20170926.xulaAtmServerController;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.net.Socket;
 
 public class ClientHandler implements Runnable {
 
     private final int TIMEOUT = 3000;
     private final Socket SOCKET;
+    private final InputStream IN;
 
     public ClientHandler(Socket socket) throws IOException {
         SOCKET = socket;
+        IN = socket.getInputStream();
     }
 
     @Override
