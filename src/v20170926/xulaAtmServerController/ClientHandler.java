effadf986bca0abef48b219795c62c7a328429e9
chris3x33 <chris3x33g@gmail.com>
2017-10-06 20:44:54 -0500

    Created Global DATA_IN:DataInputStream in ClientHandler

--- src/v20170926/xulaAtmServerController/ClientHandler.java
+++ src/v20170926/xulaAtmServerController/ClientHandler.java
@@ -1,5 +1,6 @@
 package v20170926.xulaAtmServerController;
 
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
@@ -9,10 +10,12 @@ public class ClientHandler implements Runnable {
     private final int TIMEOUT = 3000;
     private final Socket SOCKET;
     private final InputStream IN;
+    private final DataInputStream DATA_IN;
 
     public ClientHandler(Socket socket) throws IOException {
         SOCKET = socket;
         IN = socket.getInputStream();
+        DATA_IN = new DataInputStream(IN);
     }
 
     @Override
