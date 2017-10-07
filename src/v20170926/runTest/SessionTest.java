70fee486f5efa32b3fe2b2003e590a45796ea11f
chris3x33 <chris3x33g@gmail.com>
2017-10-06 20:28:50 -0500

    Added to SessionTest

--- src/v20170926/runTest/SessionTest.java
+++ src/v20170926/runTest/SessionTest.java
@@ -14,6 +14,15 @@ public class SessionTest {
         System.out.println("sessionId1 = "+sessionId1);
         System.out.println("sessionId2 = "+sessionId2);
 
+        System.out.println("SessionList Size = "+sessionList.size());
+
+        System.out.println("Exists sessionId2 = "+sessionList.sessionExists(sessionId2));
+        System.out.println("delete sessionId2 = "+sessionList.deleteSession(sessionId2));
+        System.out.println("Exists sessionId2 = "+sessionList.sessionExists(sessionId2));
+
+        System.out.println("SessionList Size = "+sessionList.size());
+
+
     }
 
 }
