36ed60da90099408b978da3e049d019e03c1428d
chris3x33 <chris3x33g@gmail.com>
2017-10-10 09:15:44 -0500

    Created Global static variables in NewUserCreator

--- src/v20170926/utils/NewUserCreator.java
+++ src/v20170926/utils/NewUserCreator.java
@@ -1,6 +1,13 @@
 package v20170926.utils;
 
+import java.util.Scanner;
+
 public class NewUserCreator {
+
+    private final static String XULA_ATM_PATH="XulaATMFiles";
+    private final static String USERLIST_PATH=XULA_ATM_PATH+"\\UserList";
+    private final static String ACCOUNTLIST_PATH=XULA_ATM_PATH+"\\AccountList";
+    private final static Scanner IN = new Scanner(System.in);
 
     public static void main(String[] args) {
 
