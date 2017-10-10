f8be8700bb4c3fe4bba66d5b10660e0433e96717
chris3x33 <chris3x33g@gmail.com>
2017-10-09 20:09:01 -0500

    Added comments to NewUserCreator

--- src/v20170926/utils/NewUserCreator.java
+++ src/v20170926/utils/NewUserCreator.java
@@ -4,5 +4,17 @@ public class NewUserCreator {
 
     public static void main(String[] args) {
 
+        //Get UserName
+
+        //Get Password
+
+        //Create UserId
+
+        //Create Checking Account
+
+        //Create Savings Account
+
+        //Write to Filesystem
+
     }
 }
