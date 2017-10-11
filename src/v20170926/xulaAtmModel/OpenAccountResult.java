27f341b865b2c6674077a5fe0884ff47536970ce
chris3x33 <chris3x33g@gmail.com>
2017-10-11 14:16:11 -0500

    Added Constructors and Getters OpenAccountResult

--- src/v20170926/xulaAtmModel/OpenAccountResult.java
+++ src/v20170926/xulaAtmModel/OpenAccountResult.java
@@ -1,4 +1,24 @@
 package v20170926.xulaAtmModel;
 
 public class OpenAccountResult extends Result {
+
+    private long userId = -1;
+
+    public OpenAccountResult(int status, String message) {
+        super(status, message);
+    }
+
+    public OpenAccountResult(Result result) {
+        super(result.getStatus(),result.getMessage());
+
+    }
+
+    public OpenAccountResult(int status, long userId) {
+        super(status);
+        this.userId = userId;
+    }
+
+    public long getUserId() {
+        return userId;
+    }
 }
