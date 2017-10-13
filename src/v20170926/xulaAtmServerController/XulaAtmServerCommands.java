9d9ea96d1862834924961dbc733022d5b7ec93cc
chris3x33 <chris3x33g@gmail.com>
2017-10-13 16:13:20 -0500

    Created Global +CREATE_NEW_USER_CMD:int in XulaAtmServerCommands

--- src/v20170926/xulaAtmServerController/XulaAtmServerCommands.java
+++ src/v20170926/xulaAtmServerController/XulaAtmServerCommands.java
@@ -3,5 +3,6 @@ package v20170926.xulaAtmServerController;
 public class XulaAtmServerCommands {
 
     public static final int LOGIN_CMD = 1;
+    public static final int CREATE_NEW_USER_CMD = 2;
 
 }
