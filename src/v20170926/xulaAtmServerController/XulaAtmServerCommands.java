a0d5be6c85bb563b2088435fc0ec703fc0bf952d
chris3x33 <chris3x33g@gmail.com>
2017-10-15 09:11:28 -0500

    Created Global +LOGOUT_CMD:int in XulaAtmServerCommands

--- src/v20170926/xulaAtmServerController/XulaAtmServerCommands.java
+++ src/v20170926/xulaAtmServerController/XulaAtmServerCommands.java
@@ -4,5 +4,6 @@ public class XulaAtmServerCommands {
 
     public static final int LOGIN_CMD = 1;
     public static final int CREATE_NEW_USER_CMD = 2;
+    public static final int LOGOUT_CMD = 3;
 
 }
