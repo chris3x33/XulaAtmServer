3dc79a655ce7ec09ec578b802402040519604695
chris3x33 <chris3x33g@gmail.com>
2017-10-16 14:49:35 -0500

    Created Global +GET_USER_ACCOUNTS_CMD:int in XulaAtmServerCommands

--- src/v20170926/xulaAtmServerController/XulaAtmServerCommands.java
+++ src/v20170926/xulaAtmServerController/XulaAtmServerCommands.java
@@ -6,5 +6,6 @@ public class XulaAtmServerCommands {
     public static final int CREATE_NEW_USER_CMD = 2;
     public static final int LOGOUT_CMD = 3;
     public static final int GET_USERNAME_CMD = 4;
+    public static final int GET_USER_ACCOUNTS_CMD = 5;
 
 }
