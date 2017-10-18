54ed6181fe41ffc4d2d97e3fef223b83c580c27e
chris3x33 <chris3x33g@gmail.com>
2017-10-18 08:25:55 -0500

    Created constructors in DepositResult

--- src/v20170926/xulaAtmModel/DepositResult.java
+++ src/v20170926/xulaAtmModel/DepositResult.java
@@ -1,4 +1,14 @@
 package v20170926.xulaAtmModel;
 
 public class DepositResult extends Result {
+    public DepositResult() {
+    }
+
+    public DepositResult(int status) {
+        super(status);
+    }
+
+    public DepositResult(int status, String message) {
+        super(status, message);
+    }
 }
