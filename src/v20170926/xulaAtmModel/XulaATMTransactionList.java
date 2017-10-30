5c253511dcd8fa6da6906a4bd3e5942676d94c80
chris3x33 <chris3x33g@gmail.com>
2017-10-30 14:17:58 -0500

    Created -readInTransactionsFrom(transactionListPath:String):void in XulaATMTransactionList

--- src/v20170926/xulaAtmModel/XulaATMTransactionList.java
+++ src/v20170926/xulaAtmModel/XulaATMTransactionList.java
@@ -1,18 +1,39 @@
 package v20170926.xulaAtmModel;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 
 public class XulaATMTransactionList {
 
     private ArrayList<XulaATMTransaction> atmTransactions;
 
-    public XulaATMTransactionList(String transactionListFolder) {
+    public XulaATMTransactionList(String transactionListFolder) throws FileNotFoundException {
         atmTransactions = new ArrayList<XulaATMTransaction>();
+        readInTransactionsFrom(transactionListFolder);
     }
 
-    public void recordTransaction(double amount, int type, String otherAccount, double prevAmount, String dateTime){
+    public void recordTransaction(double amount, int type, String otherAccount, double prevAmount, String dateTime) {
 
-        atmTransactions.add(new XulaATMTransaction(amount,type,otherAccount, prevAmount, dateTime));
+        atmTransactions.add(new XulaATMTransaction(amount, type, otherAccount, prevAmount, dateTime));
+
+    }
+
+    private void readInTransactionsFrom(String transactionListPath) throws FileNotFoundException {
+
+        File transactionListFolder = new File(transactionListPath);
+        if (!transactionListFolder.isDirectory()) {
+            return;
+        }
+
+        File[] transactionFiles = transactionListFolder.listFiles();
+        if (transactionFiles == null) {
+            return;
+        }
+
+        for (File transactionFile : transactionFiles) {
+
+        }
 
     }
 
