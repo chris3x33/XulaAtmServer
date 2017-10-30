38d68f6fd305e6f3d84946138094bbe3964ade57
chris3x33 <chris3x33g@gmail.com>
2017-10-30 16:47:22 -0500

    Created +getCurrentDate():String in DateTime

--- src/v20170926/utils/DateTime.java
+++ src/v20170926/utils/DateTime.java
@@ -1,9 +1,19 @@
 package v20170926.utils;
 
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class DateTime {
 
     private static final String DATE_FORMAT_PATTERN = "MM/dd/yyyy";
 
+    public static String getCurrentDate() {
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat(DATE_FORMAT_PATTERN);
+
+        return dateFormat.format(new Date());
+        
+    }
 
 }
