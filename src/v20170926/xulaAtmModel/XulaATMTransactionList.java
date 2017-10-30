0a9b1bfff6bc29b8096f1bbc55c891b533623123
chris3x33 <chris3x33g@gmail.com>
2017-10-30 15:03:43 -0500

    Created -readInTransactionsFrom(transactionFile:File):void in XulaATMTransactionList

--- src/v20170926/xulaAtmModel/XulaATMTransactionList.java
+++ src/v20170926/xulaAtmModel/XulaATMTransactionList.java
@@ -3,6 +3,7 @@ package v20170926.xulaAtmModel;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class XulaATMTransactionList {
 
@@ -42,7 +43,55 @@ public class XulaATMTransactionList {
         }
 
         for (File transactionFile : transactionFiles) {
+            readInTransactionsFrom(transactionFile);
+        }
 
+    }
+
+    private void readInTransactionsFrom(File transactionFile) throws FileNotFoundException {
+        Scanner scanner = new Scanner(transactionFile);
+
+        while (scanner.hasNextLine()) {
+
+            //read accountId
+            long accountId = scanner.nextLong();
+            scanner.next();// read " , "
+
+            //read accountId
+            long transactionId = scanner.nextLong();
+            scanner.next();// read " , "
+
+            //read amount
+            double amount = scanner.nextDouble();
+            scanner.next();// read " , "
+
+            //read type
+            int type = scanner.nextInt();
+            scanner.next();// read " , "
+
+            //read otherAccount
+            String otherAccount = scanner.next();
+            scanner.next();// read " , "
+
+            //read prevAmount
+            double prevAmount = scanner.nextDouble();
+            scanner.next();// read " , "
+
+            //read Date
+            String date = scanner.next();
+            scanner.nextLine();
+
+            XulaATMTransaction xulaATMTransaction = new XulaATMTransaction(
+                    accountId,
+                    transactionId,
+                    amount,
+                    type,
+                    otherAccount,
+                    prevAmount,
+                    date
+            );
+
+            atmTransactions.add(xulaATMTransaction);
         }
 
     }
