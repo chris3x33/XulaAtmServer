061a1332deb23d84a3aae5519cea028262ec0b46
chris3x33 <chris3x33g@gmail.com>
2017-11-07 06:51:44 -0600

    Coded WithdrawResult

--- src/v20170926/xulaAtmModel/result/WithdrawResult.java
+++ src/v20170926/xulaAtmModel/result/WithdrawResult.java
@@ -1,4 +1,26 @@
 package v20170926.xulaAtmModel.result;
 
 public class WithdrawResult extends Result {
+
+    private String withdrawMsg;
+
+    public WithdrawResult() {
+    }
+
+    public WithdrawResult(int status) {
+        super(status);
+    }
+
+    public WithdrawResult(int status, String message) {
+        super(status, message);
+    }
+
+    public String getWithdrawMsg() {
+        return withdrawMsg;
+    }
+
+    public void setWithdrawMsg(String withdrawMsg) {
+        this.withdrawMsg = withdrawMsg;
+    }
+
 }
