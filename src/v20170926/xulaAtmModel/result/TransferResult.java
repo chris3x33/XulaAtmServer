e4a4743ab67e0fb7f36f3d8d0406c7d7a105d74f
chris3x33 <chris3x33g@gmail.com>
2017-11-10 13:37:53 -0600

    Coded TransferResult

--- src/v20170926/xulaAtmModel/result/TransferResult.java
+++ src/v20170926/xulaAtmModel/result/TransferResult.java
@@ -1,4 +1,27 @@
 package v20170926.xulaAtmModel.result;
 
 public class TransferResult extends Result {
+
+    private String transferMsg;
+
+    public TransferResult() {
+    }
+
+    public TransferResult(int status) {
+        super(status);
+    }
+
+    public TransferResult(int status, String message) {
+        super(status, message);
+    }
+
+    public String getTransferMsg() {
+        return transferMsg;
+    }
+
+    public void setTransferMsg(String transferMsg) {
+        this.transferMsg = transferMsg;
+    }
+
 }
+
